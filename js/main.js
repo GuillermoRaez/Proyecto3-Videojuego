7763be31d97cfbb02a34d66af4580841bfbce3d3
guillermo <guillaumeraez@gmail.com>
2021-05-15 22:58:47 +0200

    Arena function updated, next step is the fighting function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 let equipo1 = [];
 let equipo2 = [];
+let j1 = "";
+let j2 = "";
+let a = 0;
+let b = 0;
 
-const cambiarStage = (destino) => {
-    // console.log(destino);
+const cambiarStage = (destiny) => {
+    // console.log(destiny);
     let arrStage = ["stage1","stage2","stage3","stage4"];
     
-    arrStage = arrStage.filter(val => !destino.includes(val));
+    arrStage = arrStage.filter(val => !destiny.includes(val));
 
-    document.getElementById(destino).style.display = "block";
+    document.getElementById(destiny).style.display = "block";
 
     for(let _stage of arrStage){
         document.getElementById(_stage).style.display = "none";
@@ -18,15 +22,12 @@ const cambiarStage = (destino) => {
 const elijeLuchador = (luchador) => { 
 
     if(equipo2.length < 4){
-
         if(equipo1.length < 4){
-
             equipo1.push(todosJugadores[luchador]);
-            console.log(equipo1.length)
         } else {
-
             equipo2.push(todosJugadores[luchador]);
 
+            //Una vez que haya 4 luchadores por equipo:
             if(equipo2.length == 4){
                 console.log("ESTE ES EL EQUIPO 1", equipo1);
                 console.log("ESTE ES EL EQUIPO 2", equipo2);
@@ -57,72 +58,55 @@ const fillEquipos = () => {
         <div><img class="luchador" src="img/${equipo1[2].nombre}.png" alt="luchador5"></div>
         <div><img class="luchador" src="img/${equipo1[3].nombre}.png" alt="luchador7"></div>
     </div>
-    <div><img class="Ring" src="img/fight.png" alt="lucha"></div>
+    <div><img class="Ring" src="img/wwelogo.png" alt="lucha"></div>
     <div class="equipoLuchadores">
         <div><img class="luchador" src="img/${equipo2[0].nombre}.png" alt="luchador2"></div>
         <div><img class="luchador" src="img/${equipo2[1].nombre}.png" alt="luchador4"></div>
         <div><img class="luchador" src="img/${equipo2[2].nombre}.png" alt="luchador6"></div>
         <div><img class="luchador" src="img/${equipo2[3].nombre}.png" alt="luchador8"></div>
-    </div>
-    `;
+    </div>`;
 }
 
 const arena = () => {
     let ring = document.getElementById("inRing");
 
     inRing.innerHTML = `
-    <div class="equipoLuchadores">
-        <div><img class="luchador" src="img/${equipo1[0].nombre}.png" alt="luchador1"></div> 
+     <div class="equipoLuchadores">
+        <div><img class="luchador" src="img/${equipo1[0].nombre}.png" alt="luchador1"></div>
+        <span class="textoBasico" id="vidaP1">${equipo1[0].nombre}</span> 
     </div>
 
-    <div><img class="inRing" src="img/Fist.png" alt="lucha"></div>
+    <div><img class="inRing" onclick="golpe1(),golpe2()" src="img/fistFight.png" alt="lucha"></div>
 
     <div class="equipoLuchadores">
         <div><img class="luchador" src="img/${equipo2[0].nombre}.png" alt="luchador2"></div>
+        <span class="textoBasico" id="vidaP2">${equipo2[0].nombre}</span>
     </div>`;
-
 }
 
-const fighting1 = () => {
-   
-    j1 = equipo1[0];
-    j2 = equipo1[1];
+const golpe1 = () => {
 
+    j1=equipo1[a];
+    j2=equipo2[b];
+    console.log("Luchad!");
 
-    console.log("empieza la lucha");
-
-    p1.hit(p2);
-    p2.hit(p1);
-
-    console.log(p1.vida,p2.vida);
-
-    //do{
-
-    //}while();
-    
-
+    j1.hit(j2);
 }
 
-let button = document.getElementById("musicContainer");
-let music = document.getElementById("music");
+//Intention to add some sounds on the characters selection.
 
-const playMusic = () => {
+// let sound = document.querySelector(".mouse");
 
-    music.play();
+//     sound.addEventListener("click", () => {
+//        let audio = document.createElement("audio");
+//        audio.setAttribute("src”, “/audio/undertaker.mp3");
+//        audio.play();
+//         console.log('click en imag');
+//     })
 
-    button.innerHTML = `
-    <button id="musicButton" onclick="pauseMusic()">
-       <i class="fas fa-volume-up"></i>
-    </button>`
-
-}
-
-const pauseMusic = () => {
-    
-    music.pause();
-
-    button.innerHTML = `
-    <button id="musicButton" onclick="playMusic()">
-        <i class="fas fa-volume-mute"></i>
-    </button>`
-}
+    // sound.addEventListener(“click”, () => {
+    //    let audio = document.createElement(“audio”);
+    //    audio.setAttribute(“src”, “/audios/Undertaker.mp3”);
+    //    audio.play();
+    //     console.log(‘click en imag’);
+    // })
